30f574356eaff50331042342d297533ad0e23e12
Administrator <Administrator@PX-20180819AXXW>
2018-11-22 14:48:08 +0800

    aaa

--- packages/tools/src/function/partial.js
+++ packages/tools/src/function/partial.js
@@ -1,5 +1,25 @@
 
 
-export default function partial(){
+const PLACEHOLDER='____dx_partial____' // 函数参数占位符
+function getHolder(func) {
+    var object = hasOwnProperty.call(lodash, 'placeholder') ? lodash : func;
+    return object.placeholder;
+  }
+function replaceHolders(array, placeholder) {
+    var index = -1,
+        length = array.length,
+        resIndex = 0,
+        result = [];
+
+    while (++index < length) {
+      var value = array[index];
+      if (value === placeholder || value === PLACEHOLDER) {
+        array[index] = PLACEHOLDER;
+        result[resIndex++] = index;
+      }
+    }
+    return result;
+  }
+export default function partial(func,...partials){
 
 }
--- packages/tools/src/util/constants.js
+++
@@ -1,2 +0,0 @@
-
-export const PLACEHOLDER='____dx_partial____' // 函数参数占位符
