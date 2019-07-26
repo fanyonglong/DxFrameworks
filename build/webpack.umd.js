70ecd4bf7dfa2c2a892bf9754eff6905249ee195
fanyonglong <315144604@qq.com>
2019-07-26 10:35:35 +0800

    Create webpack.umd.js

---
+++ build/webpack.umd.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+module.exports = function (env) {
+  let minimize = env && env.hasOwnProperty('minimize')
+
+  let config = {
+    mode: 'production',
+    entry: {
+      TransformationMatrix: path.resolve(__dirname, 'src', 'index.js')
+    },
+    output: {
+      path: path.join(__dirname, 'build-umd'),
+      filename: `transformation-matrix${minimize ? '.min' : ''}.js`,
+      library: 'TransformationMatrix',
+      libraryTarget: 'umd'
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.js']
+    },
+    module: {
+      rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ]
+      }]
+    },
+    optimization: {
+      minimize: Boolean(minimize)
+    }
+  }
+  return config
+}
