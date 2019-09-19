2f50ea51c7517af0a074202bfb8fe4c302c226dd
fanyonglong <315144604@qq.com>
2019-09-19 12:02:47 +0800

    Update rollup-babel7.js

--- build/rollup-babel7.js
+++ build/rollup-babel7.js
@@ -4,20 +4,31 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import builtins from 'rollup-plugin-node-builtins';
+import {version} from './package.json';
 export default {
   input: 'lib/index.js',
   output: {
     file: 'dist/tapable.js',
     format: 'umd',
-    name:"tapable"
+    name:"tapable",
+    exports:"named",
+    banner:`/**webpack tapable \n@version:${version}\n @build fyl build\n**/`,
+    footer:"",
+    intro:"var global=global||window;",
+    outro:""
+  },
+  external:{
+   // util:require('util')
   },
   plugins: [
+    builtins(),
     resolve(),
     commonjs(),
     babel({
       exclude: 'node_modules/**', // 只编译我们的源代码
       babelrc: false,
-      presets: [['@babel/preset-env', { modules: false,useBuiltIns:"usage" }]],
+      presets: [['@babel/preset-env', { modules: false,useBuiltIns:false }]],
     })
   ]
 };
