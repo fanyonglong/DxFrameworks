d453639e3a2d085ce0d3ffe9d365a13e05214b42
fanyonglong <315144604@qq.com>
2019-09-25 22:10:53 +0800

    Create runtime.js

---
+++ extensions/umi2/plugins/runtime/runtime.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+  /*
+   'patchRoutes',
+        'render',
+        'rootContainer',
+        'modifyRouteProps',
+        'onRouteChange',
+        'modifyInitialProps',
+        'initialProps',
+  */
+
+  export function onDx(value){
+
+        document.addEventListener('click',function(){
+            alert(value);
+        })
+  }
